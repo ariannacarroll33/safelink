50ecd340860342a616316cd8fc5cd5678cb18f18
Arianna Carroll <c17411804@mytudublin.ie>
2026-06-06 13:26:39 +0100

    Changed title to Arianna

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,13 +7,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Arianna</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Arianna</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
