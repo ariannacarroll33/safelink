e0f1598f6ebf7035573d94811c59e96abd66b48f
Arianna Carroll <c17411804@mytudublin.ie>
2026-06-08 15:17:50 +0100

    Update typography. Part missing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+import './theme/colors.css';
+import './theme/typography.css';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
